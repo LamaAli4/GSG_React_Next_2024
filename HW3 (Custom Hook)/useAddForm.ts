9ce70ebf427707d86753ed5f01d762687d2dd830
LamaAli4 <lama2015678@gmail.com>
2025-01-04 12:57:04 +0200

    Edit

--- HW3 (Custom Hook)/useAddForm.ts
+++ HW3 (Custom Hook)/useAddForm.ts
@@ -1,64 +1,64 @@
-// // import { useState } from "react";
-// import { IStudent } from "./types.ts";
-// import { validateStudent } from "./utils/validation.ts";
+// import { useState } from "react";
+import { IStudent } from "./types.ts";
+import { validateStudent } from "./utils/validation.ts";
 
-// const INITIAL_STUDENT: IStudent = {
-//   age: 0,
-//   coursesList: [],
-//   id: "",
-//   isGraduated: false,
-//   name: "",
-//   absents: 0,
-// };
+const INITIAL_STUDENT: IStudent = {
+  age: 0,
+  coursesList: [],
+  id: "",
+  isGraduated: false,
+  name: "",
+  absents: 0,
+};
 
-// interface UseAddFormProps {
-//   onSubmit: (std: IStudent) => void;
-// }
+interface UseAddFormProps {
+  onSubmit: (std: IStudent) => void;
+}
 
-// const useAddForm = ({ onSubmit }: UseAddFormProps) => {
-//   const [student, setStudent] = useState<IStudent>(INITIAL_STUDENT);
-//   const [errorsList, setErrorsList] = useState<string[]>([]);
-//   const [isOpen, setIsOpen] = useState(false);
+const useAddForm = ({ onSubmit }: UseAddFormProps) => {
+  const [student, setStudent] = useState<IStudent>(INITIAL_STUDENT);
+  const [errorsList, setErrorsList] = useState<string[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
 
-//   const handleChange = (field: string, value: any) => {
-//     setStudent({ ...student, [field]: value });
-//   };
+  const handleChange = (field: string, value: any) => {
+    setStudent({ ...student, [field]: value });
+  };
 
-//   const handleCoursesChange = (list: string[]) => {
-//     setStudent({ ...student, coursesList: list });
-//   };
+  const handleCoursesChange = (list: string[]) => {
+    setStudent({ ...student, coursesList: list });
+  };
 
-//   const handleSubmit = () => {
-//     const newStudent: IStudent = { ...student, id: Date.now().toString() };
+  const handleSubmit = () => {
+    const newStudent: IStudent = { ...student, id: Date.now().toString() };
 
-//     const errors = validateStudent(newStudent);
-//     if (errors.length > 0) {
-//       setErrorsList(errors);
-//     } else {
-//       setErrorsList([]);
-//       onSubmit(newStudent);
-//       handleClear();
-//     }
-//   };
+    const errors = validateStudent(newStudent);
+    if (errors.length > 0) {
+      setErrorsList(errors);
+    } else {
+      setErrorsList([]);
+      onSubmit(newStudent);
+      handleClear();
+    }
+  };
 
-//   const handleClear = () => {
-//     setStudent(INITIAL_STUDENT);
-//   };
+  const handleClear = () => {
+    setStudent(INITIAL_STUDENT);
+  };
 
-//   const toggleOpen = () => {
-//     setIsOpen(!isOpen);
-//   };
+  const toggleOpen = () => {
+    setIsOpen(!isOpen);
+  };
 
-//   return {
-//     student,
-//     errorsList,
-//     isOpen,
-//     handleChange,
-//     handleCoursesChange,
-//     handleSubmit,
-//     handleClear,
-//     toggleOpen,
-//   };
-// };
+  return {
+    student,
+    errorsList,
+    isOpen,
+    handleChange,
+    handleCoursesChange,
+    handleSubmit,
+    handleClear,
+    toggleOpen,
+  };
+};
 
-// export default useAddForm;
+export default useAddForm;
